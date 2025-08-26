49da8c5ff32c673c47421cd90ef11e71b2198d5a
Your Name <your-email@example.com>
2025-08-26 11:41:46 +0530

    Add Day12 challenge solution

---
+++ Day12/Solution.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Solution {
+    public static boolean isValid(String s) {
+        
+        if (s.length() % 2 != 0) return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } 
+            
+            else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+       
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));      
+        System.out.println(isValid("([)]"));     
+        System.out.println(isValid("[{()}]"));  
+        System.out.println(isValid(""));       
+        System.out.println(isValid("{[}"));      
+    }
+}
