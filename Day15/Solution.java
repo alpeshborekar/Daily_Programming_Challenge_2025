bd057b0b69a31ef162ed19d79877bcbc4f7bb3b5
Your Name <your-email@example.com>
2025-08-30 22:32:03 +0530

    Add Day15 challenge solution

---
+++ Day15/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int left = 0, right = 0, maxLen = 0;
+        while (right < n) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                maxLen = Math.max(maxLen, right - left + 1);
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return maxLen;
+    }
+}
